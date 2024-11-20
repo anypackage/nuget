33105bdafaa9f5053194a6c1cc0704b13bed1709
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2024-11-19 22:42:29 -0600

    Add Get-PackageSource

--- src/code/NuGetProvider.cs
+++ src/code/NuGetProvider.cs
@@ -2,10 +2,25 @@
 // You may use, distribute and modify this code under the
 // terms of the MIT license.
 
+using NuGet.Configuration;
+
 namespace AnyPackage.Provider.NuGet;
 
 [PackageProvider("NuGet")]
-public class NuGetProvider : PackageProvider
+public class NuGetProvider : PackageProvider, IGetSource
 {
+    public void GetSource(SourceRequest request)
+    {
+        var settings = Settings.LoadDefaultSettings(root: null);
+        var enabledSources = SettingsUtility.GetEnabledSources(settings);
 
+        foreach (var source in enabledSources)
+        {
+            if (request.IsMatch(source.Name))
+            {
+                var sourceInfo = new PackageSourceInfo(source.Name, source.Source, ProviderInfo);
+                request.WriteSource(sourceInfo);
+            }
+        }
+    }
 }
